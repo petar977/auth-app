3d307e7a9d21c037599a1ba100bf91c94f697957
Petar <pera.vu97s@gmail.com>
2024-06-23 22:09:40 +0200

    Add existing project files to Git

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main className="flex h-full flex-col items-center 
+    justify-center 
+    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          Auth
+        </h1>
+        <p className="text-white text-lg">Welcome to auth</p>
+        <div>
+          <LoginButton mode="modal" asChild>
+            <Button variant="secondary" size="lg">Sing in</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
+}
